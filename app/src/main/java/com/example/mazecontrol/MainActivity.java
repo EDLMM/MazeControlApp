d61838b1dea537ddedd4d38a3cf38bfeb61ba57a
liyizhi_i <liyizhi_i@didiglobal.com>
2020-03-28 12:44:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mazecontrol/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mazecontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.topbar) QMUITopBarLayout mTopBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        QMUIStatusBarHelper.translucent(this);
+        //初始化状态栏
+        View root = LayoutInflater.from(this).inflate(R.layout.activity_main, null);
+        ButterKnife.bind(this, root);
+        initTopBar();
+        setContentView(root);
+//        if (getIntent().getBooleanExtra("test_activity", false)) {
+//            Toast.makeText(this, "恢复到最近阅读(Boolean)", Toast.LENGTH_SHORT).show();
+//        }
+//        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowMazeActivity(View view){
+        ShowMazeActivity.actionStart(this,"start from mainAct");
+    }
+
+    private void initTopBar() {
+//        mTopBar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+////                overridePendingTransition(R.anim.slide_still, R.anim.slide_out_right);
+//            }
+//        });
+
+        mTopBar.setTitle("主页面");
+    }
+
+
+
+}
